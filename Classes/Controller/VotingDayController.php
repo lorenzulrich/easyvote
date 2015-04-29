a638088aa9af06fe83d1a091dc633800c8f79b88
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2015-04-29 06:24:02 +0000

    [TASK] Cleanup VotingDayController (refs #587)

--- Classes/Controller/VotingDayController.php
+++ Classes/Controller/VotingDayController.php
@@ -1,66 +1,34 @@
 <?php
 namespace Visol\Easyvote\Controller;
 
-/***************************************************************
- *  Copyright notice
+/**
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2013 Lorenz Ulrich <lorenz.ulrich@visol.ch>, visol digitale Dienstleistungen GmbH
- *  
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
-use TYPO3\CMS\Core\Utility\DebugUtility;
+ * The TYPO3 project - inspiring people to share!
+ */
+
 use Visol\Easyvote\Domain\Model\VotingProposal;
 
-/**
- *
- *
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- *
- */
 class VotingDayController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
 
 	/**
-	 * votingDayRepository
-	 *
 	 * @var \Visol\Easyvote\Domain\Repository\VotingDayRepository
 	 * @inject
 	 */
 	protected $votingDayRepository;
 
 	/**
-	 * metaVotingProposalRepository
-	 *
 	 * @var \Visol\Easyvote\Domain\Repository\MetaVotingProposalRepository
 	 * @inject
 	 */
 	protected $metaVotingProposalRepository;
-
-	/**
-	 * action list
-	 *
-	 * @unused
-	 * @return void
-	 */
-	public function listAction() {
-		$votingDays = $this->votingDayRepository->findAll();
-		$this->view->assign('votingDays', $votingDays);
-	}
 
 	/**
 	 * action showCurrentVotingDay
@@ -82,17 +50,6 @@ class VotingDayController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContro
 			$this->view->assign('requestedVotingProposal', $selectSingle);
 		}
 
-	}
-
-	/**
-	 * action show
-	 *
-	 * @unused
-	 * @param \Visol\Easyvote\Domain\Model\VotingDay $votingDay
-	 * @return void
-	 */
-	public function showAction(\Visol\Easyvote\Domain\Model\VotingDay $votingDay) {
-		$this->view->assign('votingDay', $votingDay);
 	}
 
 }
