1e121c9ea59bdb815ec5bc4751311059fc164a60
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2015-06-02 11:19:23 +0000

    [TASK] Minor code style cleanup

--- Classes/Controller/CommunityUserController.php
+++ Classes/Controller/CommunityUserController.php
@@ -1,29 +1,19 @@
 <?php
 namespace Visol\Easyvote\Controller;
 
-/***************************************************************
- *  Copyright notice
+/**
+ * This file is part of the TYPO3 CMS project.
  *
- *  (c) 2013 Lorenz Ulrich <lorenz.ulrich@visol.ch>, visol digitale Dienstleistungen GmbH
- *  
- *  All rights reserved
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
  *
- *  This script is part of the TYPO3 project. The TYPO3 project is
- *  free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 3 of the License, or
- *  (at your option) any later version.
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
  *
- *  The GNU General Public License can be found at
- *  http://www.gnu.org/copyleft/gpl.html.
- *
- *  This script is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  This copyright notice MUST APPEAR in all copies of the script!
- ***************************************************************/
+ * The TYPO3 project - inspiring people to share!
+ */
+
 use TYPO3\CMS\Core\Utility\DebugUtility;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Domain\Model\FileReference;
@@ -33,12 +23,6 @@ use Visol\Easyvote\Domain\Model\CommunityUser;
 use Visol\Easyvote\Property\TypeConverter\UploadedFileReferenceConverter;
 use TYPO3\CMS\Extbase\Property\PropertyMappingConfiguration;
 
-/**
- *
- *
- * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
- *
- */
 class CommunityUserController extends \Visol\Easyvote\Controller\AbstractController {
 
 	/**
@@ -809,7 +793,7 @@ class CommunityUserController extends \Visol\Easyvote\Controller\AbstractControl
 		$this->communityUserRepository->add($communityUser);
 		$this->persistenceManager->persistAll();
 
-		// generate optin uri
+		// generate opt-in uri
 		$arguments = array(
 			array('tx_easyvote_community' =>
 				array(
@@ -822,7 +806,7 @@ class CommunityUserController extends \Visol\Easyvote\Controller\AbstractControl
 		);
 		$optinUri = $this->uriBuilder->reset()->setUseCacheHash(FALSE)->setTargetPageUid($this->settings['communityRegistrationPid'])->setCreateAbsoluteUri(TRUE)->setArguments($arguments)->build();
 
-		// send optin mail
+		// send opt-in mail
 		/** @var \TYPO3\CMS\Fluid\View\StandaloneView $standaloneView */
 		$standaloneView = $this->objectManager->get('TYPO3\CMS\Fluid\View\StandaloneView');
 		$standaloneView->setFormat('html');
