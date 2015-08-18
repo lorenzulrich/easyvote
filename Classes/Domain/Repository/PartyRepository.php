ea6ac73a8e7484488c0b83ecb1f00af89b9d67f3
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2015-08-18 09:30:03 +0000

    [TASK] Add TYPO3_DB-based findAll method to PartyRepository (refs #879)

--- Classes/Domain/Repository/PartyRepository.php
+++ Classes/Domain/Repository/PartyRepository.php
@@ -1,5 +1,6 @@
 <?php
 namespace Visol\Easyvote\Domain\Repository;
+use TYPO3\CMS\Backend\Utility\BackendUtility;
 
 /**
  * This file is part of the TYPO3 CMS project.
@@ -31,6 +32,64 @@ class PartyRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 			$query->equals('isYoungParty', TRUE)
 		);
 		return $query->execute();
+	}
+
+	/**
+	 * Find all with TYPO3 DatabaseConnection and return it as a raw array
+	 *
+	 * @return array|NULL
+	 */
+	public function findAllAsRawArray(){
+		$tableName = 'tx_easyvote_domain_model_party';
+
+		$clause = '1=1';
+		// exclude party "Andere"
+		$clause .= ' AND uid NOT IN (28)';
+		$clause .= $this->getDeleteClauseAndEnableFieldsConstraint($tableName);
+		$clause .= ' AND (sys_language_uid IN (-1,0) OR (sys_language_uid = ' . $GLOBALS['TSFE']->sys_language_uid. ' AND l10n_parent = 0))';
+
+		$fields = ' *';
+		$parties = $this->getDatabaseConnection()->exec_SELECTgetRows($fields, $tableName, $clause, '', 'title ASC');
+		if (count($parties)) {
+			foreach ($parties as $key => $row) {
+				$parties[$key] = $this->getPageRepository()->getRecordOverlay($tableName, $row, $GLOBALS['TSFE']->sys_language_content, $GLOBALS['TSFE']->sys_language_contentOL);
+			}
+		}
+		return $parties;
+	}
+
+	/**
+	 * Build the enableFields constraint based on the current TYPO3 mode
+	 * PageRepository is not available in CLI/Backend context
+	 *
+	 * @param $tableName
+	 * @return string
+	 */
+	protected function getDeleteClauseAndEnableFieldsConstraint($tableName) {
+		if (TYPO3_MODE === 'FE') {
+			return $this->getPageRepository()->deleteClause($tableName) .
+			$this->getPageRepository()->enableFields($tableName);
+		} else {
+			return BackendUtility::deleteClause($tableName);
+		}
+	}
+
+	/**
+	 * Returns a pointer to the database.
+	 *
+	 * @return \TYPO3\CMS\Core\Database\DatabaseConnection
+	 */
+	protected function getDatabaseConnection() {
+		return $GLOBALS['TYPO3_DB'];
+	}
+
+	/**
+	 * Returns an instance of the page repository.
+	 *
+	 * @return \TYPO3\CMS\Frontend\Page\PageRepository
+	 */
+	protected function getPageRepository() {
+		return $GLOBALS['TSFE']->sys_page;
 	}
 
 }
