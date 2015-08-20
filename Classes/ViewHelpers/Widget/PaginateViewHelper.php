7ecccb310d0f0a68b20c30d890a9bbe7ef9f87d2
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2015-08-20 07:22:23 +0000

    [TASK] Document PaginateViewHelper

--- Classes/ViewHelpers/Widget/PaginateViewHelper.php
+++ Classes/ViewHelpers/Widget/PaginateViewHelper.php
@@ -1,54 +1,22 @@
 <?php
 
-/*                                                                        *
- * This script is backported from the FLOW3 package "TYPO3.Fluid".        *
- *                                                                        *
- * It is free software; you can redistribute it and/or modify it under    *
- * the terms of the GNU Lesser General Public License, either version 3   *
- *  of the License, or (at your option) any later version.                *
- *                                                                        *
- *                                                                        *
- * This script is distributed in the hope that it will be useful, but     *
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-    *
- * TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser       *
- * General Public License for more details.                               *
- *                                                                        *
- * You should have received a copy of the GNU Lesser General Public       *
- * License along with the script.                                         *
- * If not, see http://www.gnu.org/licenses/lgpl.html                      *
- *                                                                        *
- * The TYPO3 project - inspiring people to share!                         *
- *                                                                        */
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 /**
- * This ViewHelper renders a Pagination of objects.
+ * Class Tx_Easyvote_ViewHelpers_Widget_PaginateViewHelper
  *
- * = Examples =
- *
- * <code title="required arguments">
- * <f:widget.paginate objects="{blogs}" as="paginatedBlogs">
- *   // use {paginatedBlogs} as you used {blogs} before, most certainly inside
- *   // a <f:for> loop.
- * </f:widget.paginate>
- * </code>
- *
- * <code title="full configuration">
- * <f:widget.paginate objects="{blogs}" as="paginatedBlogs" configuration="{itemsPerPage: 5, insertAbove: 1, insertBelow: 0, maximumNumberOfLinks: 10}">
- *   // use {paginatedBlogs} as you used {blogs} before, most certainly inside
- *   // a <f:for> loop.
- * </f:widget.paginate>
- * </code>
- *
- * = Performance characteristics =
- *
- * In the above examples, it looks like {blogs} contains all Blog objects, thus
- * you might wonder if all objects were fetched from the database.
- * However, the blogs are NOT fetched from the database until you actually use them,
- * so the paginate ViewHelper will adjust the query sent to the database and receive
- * only the small subset of objects.
- * So, there is no negative performance overhead in using the Paginate Widget.
- *
- * @api
+ * This is an improved Paginate ViewHelper that allows specifying additionalParams
  */
 class Tx_Easyvote_ViewHelpers_Widget_PaginateViewHelper extends Tx_Fluid_Core_Widget_AbstractWidgetViewHelper {
 
@@ -78,5 +46,3 @@ class Tx_Easyvote_ViewHelpers_Widget_PaginateViewHelper extends Tx_Fluid_Core_Wi
 		return $this->initiateSubRequest();
 	}
 }
-
-?>
