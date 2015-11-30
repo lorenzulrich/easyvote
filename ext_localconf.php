421b086755ffa3c8d3dd2d373aabc58389202626
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-11-30 11:54:05 +0100

    [TASK] PSR-2 of ext_localconf and ext_tables

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,170 +1,168 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 /* Votings Dashboard */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Currentvotings',
-	array(
-		'VotingDay' => 'showCurrentVotingDay',
-		
-	),
-	// non-cacheable actions
-	array(
-	)
+    'Visol.' . $_EXTKEY,
+    'Currentvotings',
+    array(
+        'VotingDay' => 'showCurrentVotingDay',
+
+    ),
+    // non-cacheable actions
+    array()
 );
 
 /* Navigation of all Kantons */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Kantonnavigation',
-	array(
-		'Kanton' => 'kantonNavigation',
-	),
-	// non-cacheable actions
-	array(
-	)
+    'Visol.' . $_EXTKEY,
+    'Kantonnavigation',
+    array(
+        'Kanton' => 'kantonNavigation',
+    ),
+    // non-cacheable actions
+    array()
 );
 
 /* Voting proposals archive */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Archive',
-	array(
-		'MetaVotingProposal' => 'archive',
-	),
-	// non-cacheable actions
-	array(
-		'MetaVotingProposal' => 'archive',
-	)
+    'Visol.' . $_EXTKEY,
+    'Archive',
+    array(
+        'MetaVotingProposal' => 'archive',
+    ),
+    // non-cacheable actions
+    array(
+        'MetaVotingProposal' => 'archive',
+    )
 );
 
 /* Permalink for meta voting proposals */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Permalink',
-	array(
-		'VotingProposal' => 'permalink',
-	),
-	// non-cacheable actions
-	array(
-		'VotingProposal' => 'permalink',
-	)
+    'Visol.' . $_EXTKEY,
+    'Permalink',
+    array(
+        'VotingProposal' => 'permalink',
+    ),
+    // non-cacheable actions
+    array(
+        'VotingProposal' => 'permalink',
+    )
 );
 
 /* AJAX functions for community plugins */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'CommunityAjax',
-	array(
-		'VotingProposal' => 'showPollForVotingProposal,undoUserVoteForVotingProposal,voteForVotingProposal'
-	),
-	// non-cacheable actions
-	array(
-		'VotingProposal' => 'showPollForVotingProposal,undoUserVoteForVotingProposal,voteForVotingProposal'
-	)
+    'Visol.' . $_EXTKEY,
+    'CommunityAjax',
+    array(
+        'VotingProposal' => 'showPollForVotingProposal,undoUserVoteForVotingProposal,voteForVotingProposal'
+    ),
+    // non-cacheable actions
+    array(
+        'VotingProposal' => 'showPollForVotingProposal,undoUserVoteForVotingProposal,voteForVotingProposal'
+    )
 );
 
 /* Community-Plugins */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Community',
-	array(
-		'CommunityUser' => 'userOverview,userFunctions,loginPanel,noProfileNotification,notAuthenticatedModal,profilePicture,create,activate,dataCompletionRequest,appConfiguration',
-	),
-	// non-cacheable actions
-	array(
-		'CommunityUser' => 'userOverview,userFunctions,loginPanel,noProfileNotification,notAuthenticatedModal,profilePicture,create,activate,dataCompletionRequest,appConfiguration',
-	)
+    'Visol.' . $_EXTKEY,
+    'Community',
+    array(
+        'CommunityUser' => 'userOverview,userFunctions,loginPanel,noProfileNotification,notAuthenticatedModal,profilePicture,create,activate,dataCompletionRequest,appConfiguration',
+    ),
+    // non-cacheable actions
+    array(
+        'CommunityUser' => 'userOverview,userFunctions,loginPanel,noProfileNotification,notAuthenticatedModal,profilePicture,create,activate,dataCompletionRequest,appConfiguration',
+    )
 );
 
 /* Edit Profile */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Profile',
-	array(
-		'CommunityUser' => 'editProfile,updateProfile,removeProfile',
-	),
-	// non-cacheable actions
-	array(
-		'CommunityUser' => 'editProfile,updateProfile,removeProfile',
-	)
+    'Visol.' . $_EXTKEY,
+    'Profile',
+    array(
+        'CommunityUser' => 'editProfile,updateProfile,removeProfile',
+    ),
+    // non-cacheable actions
+    array(
+        'CommunityUser' => 'editProfile,updateProfile,removeProfile',
+    )
 );
 
 /* Eigener Vote-Wecker */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Notifications',
-	array(
-		'CommunityUser' => 'editNotifications,updateNotifications',
-	),
-	// non-cacheable actions
-	array(
-		'CommunityUser' => 'editNotifications,updateNotifications',
-	)
+    'Visol.' . $_EXTKEY,
+    'Notifications',
+    array(
+        'CommunityUser' => 'editNotifications,updateNotifications',
+    ),
+    // non-cacheable actions
+    array(
+        'CommunityUser' => 'editNotifications,updateNotifications',
+    )
 );
 
 /* Freunde mobilisieren */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Mobilize',
-	array(
-		'Event' => 'mobilizations, save, remove',
-	),
-	// non-cacheable actions
-	array(
-		'Event' => 'mobilizations, save, remove',
-	)
+    'Visol.' . $_EXTKEY,
+    'Mobilize',
+    array(
+        'Event' => 'mobilizations, save, remove',
+    ),
+    // non-cacheable actions
+    array(
+        'Event' => 'mobilizations, save, remove',
+    )
 );
 
 /* Unsubscribe */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Unsubscribe',
-	array(
-		'CommunityUser' => 'unsubscribeFromNotification',
-	),
-	// non-cacheable actions
-	array(
-		'CommunityUser' => 'unsubscribeFromNotification',
-	)
+    'Visol.' . $_EXTKEY,
+    'Unsubscribe',
+    array(
+        'CommunityUser' => 'unsubscribeFromNotification',
+    ),
+    // non-cacheable actions
+    array(
+        'CommunityUser' => 'unsubscribeFromNotification',
+    )
 );
 
 /* Party functions */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Partyfunctions',
-	array(
-		'Party' => 'manageMembers,memberFilter,listMembersByDemand',
-		'PartyMember' => 'confirm,decline,grantAdmin,remove',
-	),
-	// non-cacheable actions
-	array(
-		'Party' => 'manageMembers,listMembersByDemand',
-		'PartyMember' => 'confirm,decline,grantAdmin,remove',
-	)
+    'Visol.' . $_EXTKEY,
+    'Partyfunctions',
+    array(
+        'Party' => 'manageMembers,memberFilter,listMembersByDemand',
+        'PartyMember' => 'confirm,decline,grantAdmin,remove',
+    ),
+    // non-cacheable actions
+    array(
+        'Party' => 'manageMembers,listMembersByDemand',
+        'PartyMember' => 'confirm,decline,grantAdmin,remove',
+    )
 );
 
 /* Election supporter functions */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'Visol.' . $_EXTKEY,
-	'Electionsupporterfunctions',
-	array(
-		'ElectionSupporter' => 'electionSupporterDirectory,filter,listByDemand,follow,unfollow,wall,createEvent',
-	),
-	// non-cacheable actions
-	array(
-		'ElectionSupporter' => 'electionSupporterDirectory,filter,listByDemand,follow,unfollow,createEvent',
-	)
+    'Visol.' . $_EXTKEY,
+    'Electionsupporterfunctions',
+    array(
+        'ElectionSupporter' => 'electionSupporterDirectory,filter,listByDemand,follow,unfollow,wall,createEvent',
+    ),
+    // non-cacheable actions
+    array(
+        'ElectionSupporter' => 'electionSupporterDirectory,filter,listByDemand,follow,unfollow,createEvent',
+    )
 );
 
 /* Command Controllers */
 if (TYPO3_MODE === 'BE') {
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Visol\\Easyvote\\Command\\SmsMessageProcessorCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Visol\\Easyvote\\Command\\EmailMessageProcessorCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Visol\\Easyvote\\Command\\ImportCommandController';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Visol\\Easyvote\\Command\\SmsMessageProcessorCommandController';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Visol\\Easyvote\\Command\\EmailMessageProcessorCommandController';
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Visol\\Easyvote\\Command\\ImportCommandController';
 }
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerTypeConverter('Visol\\Easyvote\\Property\\TypeConverter\\UploadedFileReferenceConverter');
--- ext_tables.php
+++ ext_tables.php
@@ -1,54 +1,54 @@
 <?php
 if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
+    die ('Access denied.');
 }
 
 /* Votings Dashboard */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Currentvotings',
-	'easyvote Abstimmungen: Aktuelle Abstimmungen',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Currentvotings',
+    'easyvote Abstimmungen: Aktuelle Abstimmungen',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 /* Navigation of all Kantons */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Kantonnavigation',
-	'easyvote Abstimmungen/Wahlen: Kantons-Navigation',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Kantonnavigation',
+    'easyvote Abstimmungen/Wahlen: Kantons-Navigation',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 /* Voting proposals archive */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Archive',
-	'easyvote Abstimmungen: Archiv',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Archive',
+    'easyvote Abstimmungen: Archiv',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 /* Permalink for meta voting proposals */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Permalink',
-	'easyvote Abstimmungen: Permalink',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Permalink',
+    'easyvote Abstimmungen: Permalink',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 /* AJAX functions for community plugins */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'CommunityAjax',
-	'easyvote Community: AJAX-Funktionen',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'CommunityAjax',
+    'easyvote Community: AJAX-Funktionen',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 /* Community-Plugins */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Community',
-	'easyvote Community: Widgets',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Community',
+    'easyvote Community: Widgets',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 $pluginSignature = str_replace('_', '', $_EXTKEY) . '_community';
@@ -57,10 +57,10 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignat
 
 /* Profil bearbeiten */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Profile',
-	'easyvote Community: Profil bearbeiten',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Profile',
+    'easyvote Community: Profil bearbeiten',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 $pluginSignature = str_replace('_', '', $_EXTKEY) . '_profile';
@@ -70,34 +70,34 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignat
 
 /* Eigener Vote-Wecker */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Notifications',
-	'easyvote Community: Eigener Vote-Wecker',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Notifications',
+    'easyvote Community: Eigener Vote-Wecker',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 /* Freunde mobilisieren */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Mobilize',
-	'easyvote Community: Freunde mobilisieren',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Mobilize',
+    'easyvote Community: Freunde mobilisieren',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 /* Unsubscribe */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Unsubscribe',
-	'easyvote Community: Vote-Wecker abmelden',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Unsubscribe',
+    'easyvote Community: Vote-Wecker abmelden',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 /* Party functions */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Partyfunctions',
-	'easyvote Community: Parteifunktionen',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Partyfunctions',
+    'easyvote Community: Parteifunktionen',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 $pluginSignature = str_replace('_', '', $_EXTKEY) . '_partyfunctions';
@@ -106,10 +106,10 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignat
 
 /* Election supporter functions */
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-	$_EXTKEY,
-	'Electionsupporterfunctions',
-	'easyvote Community: Wahlhelfer-Funktionen',
-	'EXT:easyvote/ext_icon.gif'
+    $_EXTKEY,
+    'Electionsupporterfunctions',
+    'easyvote Community: Wahlhelfer-Funktionen',
+    'EXT:easyvote/ext_icon.gif'
 );
 
 $pluginSignature = str_replace('_', '', $_EXTKEY) . '_electionsupporterfunctions';
@@ -121,52 +121,52 @@ $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignat
 
 if (TYPO3_MODE == 'BE') {
 
-	/* Allow all tables on standard pages */
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_metavotingproposal');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_votingproposal');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_poll');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_kanton');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_votingday');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_city');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_language');
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_messagingjob');
+    /* Allow all tables on standard pages */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_metavotingproposal');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_votingproposal');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_poll');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_kanton');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_votingday');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_city');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_language');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_easyvote_domain_model_messagingjob');
 
-	// Add easyvote main module before 'user'
-	// There are not API for doing this... ;(
-	if (!isset($GLOBALS['TBE_MODULES']['easyvote'])) {
-		$modules = array();
-		foreach ($GLOBALS['TBE_MODULES'] as $key => $val) {
-			if ($key == 'user') {
-				$modules['easyvote'] = '';
-			}
-			$modules[$key] = $val;
-		}
-		$GLOBALS['TBE_MODULES'] = $modules;
-		\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
-			'easyvote',
-			'',
-			'',
-			\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('easyvote') . 'mod/easyvote/');
-	}
+    // Add easyvote main module before 'user'
+    // There are not API for doing this... ;(
+    if (!isset($GLOBALS['TBE_MODULES']['easyvote'])) {
+        $modules = array();
+        foreach ($GLOBALS['TBE_MODULES'] as $key => $val) {
+            if ($key == 'user') {
+                $modules['easyvote'] = '';
+            }
+            $modules[$key] = $val;
+        }
+        $GLOBALS['TBE_MODULES'] = $modules;
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addModule(
+            'easyvote',
+            '',
+            '',
+            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('easyvote') . 'mod/easyvote/');
+    }
 
-	/* Easyvote Backend Module */
-	\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-		'Visol.' . $_EXTKEY,
-		'easyvote',
-		'easyvote',
-		'top',
-		array(
-			'CommunityUser' => 'backendDashboard,backendSmsMessagingIndex,backendSmsMessageSend,backendEmailExportIndex,backendEmailExportPerform'
-		),
-		array(
-			'access' => 'user,group',
-			'icon' => 'EXT:easyvote/Resources/Public/Icons/votewecker.png',
-			'labels' => 'LLL:EXT:easyvote/Resources/Private/Language/votewecker_module.xlf'
-		)
-	);
+    /* Easyvote Backend Module */
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'Visol.' . $_EXTKEY,
+        'easyvote',
+        'easyvote',
+        'top',
+        array(
+            'CommunityUser' => 'backendDashboard,backendSmsMessagingIndex,backendSmsMessageSend,backendEmailExportIndex,backendEmailExportPerform'
+        ),
+        array(
+            'access' => 'user,group',
+            'icon' => 'EXT:easyvote/Resources/Public/Icons/votewecker.png',
+            'labels' => 'LLL:EXT:easyvote/Resources/Private/Language/votewecker_module.xlf'
+        )
+    );
 
-	// Default User TSConfig to be added in any case.
-	TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
+    // Default User TSConfig to be added in any case.
+    TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
 		# User TSconfig defining default pid for "fe_users" in Vidi:
 		tx_vidi {
 			dataType {
