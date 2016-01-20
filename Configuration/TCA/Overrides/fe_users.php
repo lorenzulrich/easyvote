fce4e522e913b5190f19202256b0e20b71853ad7
Lorenz Ulrich <lorenz.ulrich@visol.ch>
2016-01-20 15:04:55 +0000

    [TASK] Move all Vidi configuration for fe_users to EXT:easyvote (refs #1454)
    Was accidentially placed in EXT:easyvote_education.

--- Configuration/TCA/Overrides/fe_users.php
+++ Configuration/TCA/Overrides/fe_users.php
@@ -331,21 +331,76 @@ $GLOBALS['TCA']['fe_users']['ctrl']['label_alt'] = 'last_name,first_name';
 $GLOBALS['TCA']['fe_users']['ctrl']['label_alt_force'] = TRUE;
 
 
-$tca = array(
-    'grid' => array(
-        'columns' => array(
-            'party_admin_allowed_cantons' => array(
-                'sortable' => FALSE,
-                'renderers' => array(
+
+// Remove all facets first
+unset($GLOBALS['TCA']['fe_users']['grid']['facets']);
+
+// Exclude more fields from TCA.
+$tca = [
+    'grid' => [
+        'excluded_fields' => $GLOBALS['TCA']['fe_users']['grid']['excluded_fields'] . ', uid, name, crdate, tstamp, password, middle_name, address, fax, title, zip, city, country, www, company, fal_image, starttime, endtime, lastlogin, tx_extbase_type, customer_number, age_start, age_end, datasets, salutation, kanton, notification_mail_active, notification_sms_active, notification_related_users, community_user, party_verification_code,tx_cabagloginas_loginas, tx_easyvoteeducation_panels',
+        'columns' => [
+            'party_admin_allowed_cantons' => [
+                'sortable' => false,
+                'renderers' => [
                     'Fab\Vidi\Grid\RelationEditRenderer',
                     'Fab\Vidi\Grid\RelationRenderer',
-                ),
+                ],
+            ],
+        ],
+        'facets' => [
+            'uid',
+            'username',
+            'first_name',
+            'last_name',
+            'email',
+            'usergroup',
+            new \Fab\Vidi\Facet\StandardFacet(
+                'privacy_protection',
+                'LLL:EXT:easyvote/Resources/Private/Language/locallang_db.xlf:tx_easyvote_domain_model_communityuser.privacy_protection',
+                [
+                    '1' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.0',
+                    '0' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.1'
+                ]
             ),
-        ),
-    ),
-);
+            new \Fab\Vidi\Facet\StandardFacet(
+                'notification_mail_active',
+                'LLL:EXT:easyvote/Resources/Private/Language/locallang_db.xlf:tx_easyvote_domain_model_communityuser.notification_mail_active',
+                [
+                    '1' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.0',
+                    '0' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.1'
+                ]
+            ),
+            new \Fab\Vidi\Facet\StandardFacet(
+                'notification_sms_active',
+                'LLL:EXT:easyvote/Resources/Private/Language/locallang_db.xlf:tx_easyvote_domain_model_communityuser.notification_sms_active',
+                [
+                    '1' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.0',
+                    '0' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.1'
+                ]
+            ),
+            new \Fab\Vidi\Facet\StandardFacet(
+                'community_news_mail_active',
+                'LLL:EXT:easyvote/Resources/Private/Language/locallang_db.xlf:tx_easyvote_domain_model_communityuser.community_news_mail_active',
+                [
+                    '1' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.0',
+                    '0' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.1'
+                ]
+            ),
+            new \Fab\Vidi\Facet\StandardFacet(
+                'disable',
+                'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active',
+                [
+                    '0' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.0',
+                    '1' => 'LLL:EXT:vidi/Resources/Private/Language/locallang.xlf:active.1'
+                ]
+            ),
+            new \Fab\Vidi\Facet\StandardFacet(
+                'city_selection',
+                'LLL:EXT:easyvote/Resources/Private/Language/locallang_db.xlf:tx_easyvote_domain_model_communityuser.city_selection'
+            ),
+        ],
+    ]];
+
 
 \TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule($GLOBALS['TCA']['fe_users'], $tca);
-
-
-
